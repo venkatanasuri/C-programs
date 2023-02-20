d5184350b2654d74b620d7da0fbbe7013034b1e4
venkatanasuri <125712483+venkatanasuri@users.noreply.github.com>
2023-02-20 21:55:17 +0530

    Add files via upload

---
+++ Day17(3).cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int num, is_prime = 1; 
+    scanf("%d", &num);
+
+    if (num <= 1) { 
+        is_prime = 0; 
+    } else {
+        for (int i = 2; i <= num/2; i++) {
+            if (num % i == 0) { 
+                is_prime = 0; 
+                break;
+            }
+        }
+    }
+
+    if (is_prime) {
+        printf("%d is a prime number.\n", num);
+    } else {
+        printf("%d is not a prime number.\n", num);
+    }
+
+    return 0;
+}
